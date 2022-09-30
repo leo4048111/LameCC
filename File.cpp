671910a07d26a2df28eedfc2cc15ebabec175a80
Potato5K <74029782+leo4048111@users.noreply.github.com>
2022-09-30 10:57:11 +0800

    Added nextLine method
    Now file is read line by line into char buffer instead of being read only once as a whole.

--- File.cpp
+++ File.cpp
@@ -3,16 +3,26 @@
 namespace cc
 {
     File::File(std::string path):
-    _pos({1, 1})
+    _pos({0, 1})
     {
         _ifs = std::ifstream(path);
+    }
+
+    void File::nextLine()
+    {
         std::string line;
-        while(std::getline(_ifs, line))
-        {
+
+        // flush stringstream first
+        _ss.str("");
+
+        // update positions
+        _pos.line++;
+        _pos.column = 1;
+        _curIdx = 0;
+        if(std::getline(_ifs, line)) 
             _ss << line << '\n';
-        }
-        _ss << (char)EOF;
-        printf("%s\n", _ss.str().c_str());
+        else 
+            _ss << (char)EOF;
     }
 
     const char File::nextChar()
@@ -20,12 +30,7 @@ namespace cc
         if(_curIdx >= _ss.str().size()) return EOF;
 
         char c = _ss.str()[_curIdx++];
-        if(c == '\n')
-        {
-            _pos.line++;
-            _pos.column = 1;
-        } 
-        else _pos.column++;
+        _pos.column++;
 
         return c;
     }
@@ -34,21 +39,21 @@ namespace cc
     {
         _curIdx = (_curIdx <= 0) ? 0 : _curIdx - 1;
         char c = _ss.str()[_curIdx];
-        if(c == '\n')
-        {
-            int i = _curIdx - 1;
-            for(; i >= 0 && _ss.str()[i] != '\n'; i--);
-            _pos.line--;
-            _pos.column = _curIdx - i + (_ss.str()[i] != '\n');
-        }
-        else _pos.column--;
+        // if(c == '\n')
+        // {
+        //     int i = _curIdx - 1;
+        //     for(; i >= 0 && _ss.str()[i] != '\n'; i--);
+        //     _pos.line--;
+        //     _pos.column = _curIdx - i + (_ss.str()[i] != '\n');
+        // }
+        // else _pos.column--;
+        _pos.column = _curIdx + 1;
     }
 
     const char File::peekChar()
     {
-        if(_curIdx < _ss.str().size())
-            return _ss.str()[_curIdx];
+        if(_curIdx >= _ss.str().size()) return EOF;
         
-        return EOF;
+        return _ss.str()[_curIdx];
     }
 }
