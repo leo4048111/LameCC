ae42c055c4d57718bc3038b7a29db21698e9135a
Potato5K <74029782+leo4048111@users.noreply.github.com>
2023-02-23 12:49:05 +0800

    Fixed invalid result while reading escape characters
    Fixed invalid result while reading escape characters

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -221,6 +221,32 @@ namespace lcc
             else if (c == '\\')
             {
                 c = nextChar();
+                switch(c)
+                {
+                    case 'a':
+                        c = '\a';
+                        break;
+                    case 'b':
+                        c = '\b';
+                        break;
+                    case 'f':
+                        c = '\f';
+                        break;
+                    case 'n':
+                        c = '\n';
+                        break;
+                    case 'r':
+                        c = '\r';
+                        break;
+                    case 't':
+                        c = '\t';
+                        break;
+                    case 'v':
+                        c = '\v';
+                        break;
+                    default:
+                        break;
+                }
             }
             buffer += c;
         }
